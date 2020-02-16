96c4138bf325e11e13a7f2cafdd4bc52996f1051
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2020-02-15 22:12:31 -0300

    create schema

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const myApp = require('./myApp');
+const json = require('./data/db')
 const port = 3001;
 
 mongoose.Promise = global.Promise;
@@ -26,6 +27,52 @@ mongoose.connect('mongodb://localhost:27017/test_com_8_3')
         matiasUser.save(() => {
             console.log('User was added');
         });
+
+        //Define Schema comments
+        const commentSchema = mongoose.Schema({
+            username: String,
+            email: String,
+            comment: String
+        });
+        //Pasa schema a model
+        const Comment = mongoose.model('Comment', commentSchema);
+        json.comments.forEach(data => {
+            //Nueva instancia del documento
+            const commen = new Comment({
+                _id: mongoose.Types.ObjectId(),
+                username: data.username,
+                email: data.email,
+                comment: data.comment
+            });
+            commen.save(() => {
+                console.log('Comment was added');
+            });
+        });
+        //Define Schema articles
+        const articlesSchema = mongoose.Schema({
+            title: String,
+            date: Date,
+            copete: String,
+            image: String,
+            description: String
+        });
+        //Pasa Shema a model
+        const Articles = mongoose.model('Articles', articlesSchema);
+        json.articles.forEach(items => {
+            //Nueva instancia del documento
+            const article = new Articles({
+                _id: mongoose.Types.ObjectId(),
+                title: items.title,
+                date: new Date(),
+                copete: items.copete,
+                image: items.image,
+                description: items.description
+            })
+            article.save(() => {
+                console.log('Article was added')
+            });
+        })
+
         myApp.listen(port, () => {
             console.log(`Server running on http://localhost:${port}...`);
         });
